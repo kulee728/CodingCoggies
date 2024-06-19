077449f5ebfcd28a4cef8b7b6c7c404e65e6fbb3
kulee728 <kulee728@gmail.com>
2024-06-19 14:58:37 +0900

    project start

---
+++ CoddingCoggies/src/main/java/coddingcoggies/CoddingCoggiesApplication.java
@@ -0,0 +1,13 @@
+package coddingcoggies;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoddingCoggiesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoddingCoggiesApplication.class, args);
+	}
+
+}
